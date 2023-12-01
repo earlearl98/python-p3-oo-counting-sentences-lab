573eb254baef63463460b98c6cf0a2a82bbfffc4
earlearl98 <earlearl1720@gmail.com>
2023-12-01 17:24:12 +0300

    Completed assignment

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,28 @@
-#!/usr/bin/env python3
+import re
 
 class MyString:
-  pass
+    def __init__(self, value=""):
+        # Initialize with an empty string if no value is provided
+        if not isinstance(value, str):
+            raise ValueError("The value must be a string.")
+        
+        self.value = value
+
+    def is_sentence(self):
+        # Check if the value ends with a period
+        return self.value.endswith('.')
+
+    def is_question(self):
+        # Check if the value ends with a question mark
+        return self.value.endswith('?')
+
+    def is_exclamation(self):
+        # Check if the value ends with an exclamation mark
+        return self.value.endswith('!')
+
+    def count_sentences(self):
+        # Split the value into sentences based on '.', '?', and '!'
+        sentences = [sentence.strip() for sentence in re.split(r'[.!?]', self.value) if sentence]
+        return len(sentences)
+
+
